e7b38c27a922ea285a7eee9c0d8932653945ed77
conne <conne@host.docker.internal>
2021-10-08 17:05:49 +0530

    Remove unwanted class

--- task/src/task/Test.java
+++ task/src/task/Test.java
@@ -2,13 +2,10 @@ package task;
 
 public class Test {
 
-	public Test() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
          System.out.println("Do you know who i am ?");
+         
 	}
 
 }
